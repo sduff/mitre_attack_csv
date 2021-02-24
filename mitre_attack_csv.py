15e8cf501f5ec03a4ae3c959402208518c1d8076
Simon Duff <simon.duff@gmail.com>
2021-02-25 10:14:02 +1100

    de-HTML output and added logging

--- mitre_attack_csv.py
+++ mitre_attack_csv.py
@@ -4,12 +4,15 @@ import csv
 import json
 import requests
 
+
 url = "https://github.com/mitre/cti/raw/master/enterprise-attack/enterprise-attack.json"
 outfile = "enterprise-attack.csv"
 
+print("Fetching latest enterprise-attack.json ...")
 d = requests.get(url)
 assert (d.status_code==200),"Failure fetching url"
 
+print("Parsing file ...")
 j = d.json()
 assert ('spec_version' in j), "Failure reading version info in JSON file"
 assert ('objects' in j), "Failure reading objects in JSON file"
@@ -30,6 +33,8 @@ for i in j['objects']:
 	if t not in o: o[t] = {}
 	o[t][id] = i
 
+print("Generating list of tactics ...")
+
 # Generate a list of tactics
 tactics = {}
 for t in o['x-mitre-tactic']:
@@ -38,11 +43,11 @@ for t in o['x-mitre-tactic']:
 	id = o['x-mitre-tactic'][t]['external_references'][0]["external_id"]
 	url = o['x-mitre-tactic'][t]['external_references'][0]["url"]
 
-	tactics[short_name] = (name, id, url)
+	tactics[short_name] = name
 
 # minature markdown
 import re
-def minimd(s):
+def minimd(s,fmt="text"):
 
 	code = re.compile('<code>(?P<codeblock>.*?)</code>')
 
@@ -50,21 +55,36 @@ def minimd(s):
 	link = re.compile('\[([^[]*?)\]\((.*?)\)')
 	header = re.compile('(?:^|\n)#+([^\n]*)')
 
-	s = code.sub(lambda x: '<code>{}</code>'.format(x.group('codeblock').replace('<','&lt;')), s)
-	s = bold.sub(r'<b>\1</b>',s)
-	s = link.sub(r'<a href="\2">\1</a>', s)
-	s = header.sub(r'<b><u>\1</u></b><br/>',s)
+	if fmt=="html":
+		s = code.sub(lambda x: '<code>{}</code>'.format(x.group('codeblock').replace('<','&lt;')), s)
+		s = bold.sub(r'<b>\1</b>',s)
+		s = link.sub(r'<a href="\2">\1</a>', s)
+		s = header.sub(r'<b><u>\1</u></b><br/>',s)
 
-	# rewrite links to mitre page to this one (mitre to internal link)
-	mtil = re.compile('"https://attack.mitre.org/techniques/(?P<technique>.*?)"')
-	s = mtil.sub(lambda x: '"#{}"'.format(x.group('technique').replace('/','.')), s)
+		# rewrite links to mitre page to this one (mitre to internal link)
+		mtil = re.compile('"https://attack.mitre.org/techniques/(?P<technique>.*?)"')
+		s = mtil.sub(lambda x: '"#{}"'.format(x.group('technique').replace('/','.')), s)
 
-	s = s.replace('\n','<br/>')
+		s = s.replace('\n','<br/>')
 
-	#s = f"{s[:100]}..." if len(s)>=100 else s
+	elif fmt=="text":
+		# tidy headers
+		s = header.sub(r'# \1 #\n',s)
+	
+		# neaten code
+		s = code.sub(lambda x: '`{}`'.format(x.group('codeblock')), s)
+
+		# rewrite links to mitre page to plaintext
+		mtil = re.compile('https://attack.mitre.org/(techniques|tactics|software)/(?P<technique>[^\])"]+)')
+		s = mtil.sub(lambda x: '{}'.format(x.group('technique').replace('/','.')), s)
+
+		# remove <br>
+		s = s.replace('<br>','\n')
+		
 
 	return s
 
+print("Generating list of techniques ...")
 # Generate a list of techniques
 tech = {}
 for tn in o['attack-pattern']:
@@ -82,7 +102,7 @@ for tn in o['attack-pattern']:
 	name = t['name'] if 'name' in t else ""
 	platforms = t['x_mitre_platforms'] if 'x_mitre_platforms' in t else []
 	kill_chain_phases = t['kill_chain_phases'] if 'kill_chain_phases' in t else []
-	kill_chain_phases = [x['phase_name'] for x in kill_chain_phases if x['kill_chain_name']=="mitre-attack"]
+	kill_chain_phases = [tactics[x['phase_name']] for x in kill_chain_phases if x['kill_chain_name']=="mitre-attack"]
 	data_sources = t['x_mitre_data_sources'] if 'x_mitre_data_sources' in t else [] 
 	description = t['description'] if 'description' in t else ""
 	description = minimd(description)
@@ -91,7 +111,7 @@ for tn in o['attack-pattern']:
 
 	tech[mitre_id] = (name, tn, mitre_url, platforms, kill_chain_phases, data_sources, detection, description)
 
-
+print("Generating CSV file ...")
 with open(outfile,'w',newline='\n') as out:
 	writer = csv.DictWriter(out, ['name', 'id', 'url', 'platforms', 'kill chain phases', 'description', 'data sources', 'detection'], quoting=csv.QUOTE_ALL)
 	writer.writeheader()	
